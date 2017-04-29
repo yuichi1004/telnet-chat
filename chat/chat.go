dbbff79057ef31c1d5aa3941e89993d0984bd0a1
Yuichi Murata <yuichi.murata@dena.com>
2017-04-29 11:36:41 +0900

    specify chat interface

---
+++ chat/chat.go
@@ -0,0 +1,38 @@
+package chat
+
+type Room struct {
+	Name string
+	Participants []string
+}
+
+type Chat interface {
+	// Create new chat room
+	NewRoom(name string) error
+
+	// Get list of rooms
+	GetRooms() ([]string, error)
+
+	// Get specific room
+	GetRoom(room string) (*Room, error)
+	
+	// Join to a chat room as a participant
+	Join(room, user string) (Participant, error)
+
+	// connect a user
+	Connect(user string) error
+
+	// diconnect a user
+	Disconnect(user string) error
+}
+
+
+type Participant interface {
+	// Send a message as a participant
+	Send(message string) error
+
+	// Subscribe messages on the chat room
+	Subscribe() (ch chan string, err error)
+
+	// Leave the chat room
+	Leave() error
+}
